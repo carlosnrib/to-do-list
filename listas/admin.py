fb2023bb11c0274ef62f6296a510474616a281b2
carlosnrib <carlosnribeiro3@gmail.com>
2022-10-26 08:44:25 -0300

    Configurações do django admin

--- listas/admin.py
+++ listas/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Listas, Tarefa
 
 # Register your models here.
+
+class tarefaAdmin(admin.ModelAdmin):
+    list_display = [
+        'descricao',
+        'feita',
+        'prioridade',
+        'data',
+        'lista'
+    ]
+
+    list_filter = [
+        'feita',
+        'prioridade',
+        'data'
+    ]
+
+    search_fields = [
+        'descricao'
+    ]
+
+admin.site.register(Listas)
+admin.site.register(Tarefa, tarefaAdmin)
